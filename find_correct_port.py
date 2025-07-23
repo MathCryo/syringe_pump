a8a6c51484f07304d86ab8de04303fcaf3964246
josephabrams <jsa967@usask.ca>
2025-07-23 12:27:18 -0600

    adjusted so it now works with elevated permissions and throws appropriate error message, minor corrections

--- find_correct_port.py
+++ find_correct_port.py
@@ -3,72 +3,62 @@ import os
 import serial  # include pySerial 
 import serial.tools.list_ports  # tool to find list_ports
 import time
+import stat
 
-def decode_NE1000_responses(self, response: str)->str:
-    start_char="\x02"
-    end_char="\x03"
-    resp=response.decode("utf-8", "ignore")
-    """ The following character adjustments make the responses easier to read and
-    clue the user into any errors. They should be in a dictionary or list to make code cleaner,
-    but here they are.
-    I is infused/infusing
-    W is withdrew/withdrawing
-    P is paused
-    S is just a good command recieved"""
-    resp=resp.replace(end_char,")")
-    resp=resp.replace(start_char,"(")
-    resp=resp.replace('S',f" pump: ")
-    resp=resp.replace('I'," I ")
-    resp=resp.replace('W'," W ")
-    resp=resp.replace('P', " P ")
-    resp=resp.replace('NA', " warning! Command not applicable ")
-    resp=resp.replace('?', " Error in Command ")
-    resp=resp.replace('COM', "Invalid packet")
-    resp=resp.replace('OOR', "Command data is out of range")
-    resp=resp.replace('IGN', "Command ignored due to a simultaneous new Phase start")
-    return resp
-## create a test instance to brute force try ports until a connection is sucessfull
+def write_to_NE_and_get_response( ser: serial, *messages: str):
+    message_to_network = ""
+    for message in messages:
+        if message and isinstance(message, str):
+            message_to_network += message
+    ser.write(message_to_network.encode('utf-8'))
+    time.sleep(0.05)  # Slight delay for pump processing
+    response = ser.read(ser.in_waiting).decode('utf-8', 'ignore')
+    return response.strip()
+
 def pump_instance_test():
     port_list=serial.tools.list_ports.comports()
-    correct_port="port not found"
+    correct_port=None
+    lock_count=0
     for name in sorted(port_list):
         port_name=name[0]
-        print("Trying port: ", port_name)
+        # print("Trying port: ", port_name)
         try:
             print("Trying port: ", port_name)
+            os.chmod(port_name,stat.S_IRGRP )
+            # os.chmod(port_name,stat.S_IXGRP )
+            os.chmod(port_name,stat.S_IWGRP )
             ser=serial.Serial(port=str(port_name), baudrate=19200, timeout=5, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE )
-            ser.open()
             if ser.is_open:
+                print("Connected to PORT ON: ", port_name, "Checking for NE pump firmware...")
                 #try all possible addresses
                 for n in range(10):
                     ser.reset_input_buffer()
                     pumpAddress = f"{n:02}"
-                    test_message = f"{pumpAddress} VER \r"
-                    ser.write(test_message.encode('utf-8'))
-                    time.sleep(0.1)
-                    raw_response=ser.read(ser.in_waiting)
-                    response=decode_NE1000_responses(raw_response)
+                    test_message = f"{pumpAddress} VER\r"
+                    # print(test_message.encode('utf-8'))
+                    raw_response=write_to_NE_and_get_response(ser,test_message)
+                    # print(raw_response)
                     if "NE" in response:
                         correct_port=port_name
                 ser.close()
+        except PermissionError as err:
+            lock_count=lock_count+1
+            # print(err)
         except Exception as err:
-            print("tested: ", port_name)
-            print(err)
+            # print("tested: ", port_name, " it was not correct.")
+            # print(err)
             continue
         else:
-            print("tested: ", port_name)
+            # print("tested: ", port_name, " it was not correct.")
             continue
-    print(correct_port)
+    if correct_port:
+        print("Correct port is: ", correct_port)
+    else:
+        print("Correct port not found, check connection and permissions!")
+        if lock_count== len(port_list):
+            print("Port is likely lacking read-write access, either set permissions for the port or run as Administrator!")
     return correct_port
-# def auto_detect_syringe_pump(port_value):
-#     ports = serial.tools.list_ports.grep("Prolific")
-#     for port, description, hardware in sorted(ports):
-#         port_value = port
-#     return str(port_value)
 def main():
-    # iter_list=serial.tools.list_ports.comports()
-    # for i in sorted(iter_list):
-    # print(iter_list[0][0])
     pump_instance_test()
 
 if __name__ == "__main__":
